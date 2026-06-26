dc3abe99a5a2808aa4f7ec5a858ca6e76c7aff40
avinashrj-del <avinashrajwade.in@gmail.com>
2026-06-26 22:58:26 +0530

    Implemented new students feature

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 #include <vector>
 #include <string>
 using namespace std;
+
+
 struct Student {
     int id;
     string name;
     int age;
     float marks;
 };
+
+
+// Phase 5: Add Student
 void addStudent(vector<Student>& students){
 
     Student s;
@@ -31,12 +36,40 @@ void addStudent(vector<Student>& students){
 }
 
 
+// Phase 6: View Students
+void viewStudents(vector<Student>& students){
+
+    if(students.empty()){
+        cout << "No students available\n";
+        return;
+    }
+
+
+    cout << "\n===== Student List =====\n";
+
+
+    for(Student s : students){
+
+        cout << "ID: " << s.id << endl;
+        cout << "Name: " << s.name << endl;
+        cout << "Age: " << s.age << endl;
+        cout << "Marks: " << s.marks << endl;
+
+        cout << "----------------------\n";
+    }
+}
+
+
+
 int main() {
+
     vector<Student> students;
 
     int choice;
 
+
     do {
+
         cout << "\n===== Student Management System =====\n";
         cout << "1. Add Student\n";
         cout << "2. View Students\n";
@@ -45,41 +78,51 @@ int main() {
         cout << "5. Delete Student\n";
         cout << "6. Exit\n";
 
+
         cout << "Enter Choice: ";
         cin >> choice;
 
-        switch(choice) {
-            case 1:
 
-    
-    addStudent(students);
-    break;
+        switch(choice){
+
+            case 1:
+                addStudent(students);
+                break;
+
 
             case 2:
-                cout << "View Students Selected\n";
+                viewStudents(students);
                 break;
+
 
             case 3:
                 cout << "Search Student Selected\n";
                 break;
 
+
             case 4:
                 cout << "Update Student Selected\n";
                 break;
+
 
             case 5:
                 cout << "Delete Student Selected\n";
                 break;
 
+
             case 6:
                 cout << "Exiting...\n";
                 break;
+
 
             default:
                 cout << "Invalid Choice\n";
         }
 
+
     } while(choice != 6);
+
+
 
     return 0;
 }
