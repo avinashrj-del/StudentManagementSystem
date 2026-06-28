f68f664d860e65c7f72a74e48b4e1e206fe2ec5f
avinashrj-del <avinashrajwade.in@gmail.com>
2026-06-28 23:48:31 +0530

    new Search feature implemented

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 
 struct Student {
+
     int id;
     string name;
     int age;
     float marks;
+
 };
 
 
@@ -33,15 +35,19 @@ void addStudent(vector<Student>& students){
     students.push_back(s);
 
     cout << "Student Added Successfully\n";
+
 }
+
 
 
 // Phase 6: View Students
 void viewStudents(vector<Student>& students){
 
     if(students.empty()){
+
         cout << "No students available\n";
         return;
+
     }
 
 
@@ -56,21 +62,69 @@ void viewStudents(vector<Student>& students){
         cout << "Marks: " << s.marks << endl;
 
         cout << "----------------------\n";
+
     }
+
 }
 
 
 
-int main() {
+// Phase 7: Search Student
+void searchStudent(vector<Student>& students){
+
+    int id;
+
+
+    cout << "Enter Student ID to search: ";
+    cin >> id;
+
+
+
+    for(Student s : students){
+
+
+        if(s.id == id){
+
+
+            cout << "\nStudent Found\n";
+
+
+            cout << "ID: " << s.id << endl;
+            cout << "Name: " << s.name << endl;
+            cout << "Age: " << s.age << endl;
+            cout << "Marks: " << s.marks << endl;
+
+
+            return;
+
+        }
+
+    }
+
+
+    cout << "Student not found\n";
+
+}
+
+
+
+
+
+int main(){
+
 
     vector<Student> students;
+
 
     int choice;
 
 
-    do {
+
+    do{
+
 
         cout << "\n===== Student Management System =====\n";
+
         cout << "1. Add Student\n";
         cout << "2. View Students\n";
         cout << "3. Search Student\n";
@@ -83,46 +137,70 @@ int main() {
         cin >> choice;
 
 
+
         switch(choice){
 
+
             case 1:
+
                 addStudent(students);
                 break;
 
 
+
             case 2:
+
                 viewStudents(students);
                 break;
 
 
+
             case 3:
-                cout << "Search Student Selected\n";
+
+                searchStudent(students);
                 break;
 
 
+
             case 4:
+
                 cout << "Update Student Selected\n";
                 break;
 
 
+
             case 5:
+
                 cout << "Delete Student Selected\n";
                 break;
 
 
+
             case 6:
+
                 cout << "Exiting...\n";
                 break;
 
 
+
             default:
+
                 cout << "Invalid Choice\n";
+
         }
 
 
-    } while(choice != 6);
+
+    }while(choice != 6);
 
 
 
     return 0;
+
 }
+
+
+
+        
+
+
